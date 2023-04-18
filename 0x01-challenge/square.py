45bdd9330dc879c11daefb19cbfdc7a5eab42126
Yosinan <ymymym4634@gmail.com>
2023-04-18 12:14:03 -0700

    Fix_My_Code_Challenge

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,40 +1,68 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
-square module contains
-    - square class
+Square Module
 """
 
 
 class Square():
     """
-    Square class
+    class that defines a square
     """
-    width = 0
-    height = 0
 
-    def __init__(self, *args, **kwargs):
-        """
-        Initializes a square
-        """
+    def __init__(self, width=0, height=0):
+        """ init method """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
-    def area_of_my_square(self):
-        """ Returns the area of the square """
-        return self.width * self.height
+    @property
+    def width(self):
+        ''' getter for width '''
+        return self.__width
 
-    def permiter_of_my_square(self):
-        """ Returns the perimeter of a square """
+    @property
+    def height(self):
+        ''' getter for height '''
+        return self.__height
+
+    @width.setter
+    def width(self, val):
+        ''' setter for width '''
+        if val < 1:
+            raise ValueError('Width can not be less than 1')
+        else:
+            self.__width = val
+
+    @height.setter
+    def height(self, val):
+        ''' setter for height '''
+        if val < 1:
+            raise ValueError('Height can not be less than 1')
+        else:
+            self.__height = val
+
+    '''
+    def __init__(self, *args, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+    '''
+
+    def area_of_my_square(self):
+        """ Area of the square """
+        return (self.width * self.height)
+
+    def PermiterOfMySquare(self):
+        ''' perimeter of square '''
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """ String representation of a square """
+        ''' str rep of the square '''
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-    """ Tests the Square class"""
+    """ test the square class wiz instances """
+
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.permiter_of_my_square())
+    print(s.PermiterOfMySquare())
